401e39e4183a9200430f52db12f9843520d19e40
ryotakoyama <kryota.gin@gmail.com>
2020-05-03 21:38:17 +0900

    HTMLElementに型アサーションを指定して .value メソッドを使用する

--- src/components/Advance/TypeAsertion.vue
+++ src/components/Advance/TypeAsertion.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>{{ getCurrentComponentName }}</div>
+  <div>
+    <p>{{ getCurrentComponentName }}</p>
+    <input id="input" ref="inputtext" />
+  </div>
 </template>
 
 <script lang="ts">
@@ -12,6 +15,19 @@ class Intersection extends Vue {
 
   get getCurrentComponentName() {
     return this.componentName;
+  }
+
+  mounted() {
+    // console.log(this.$refs);
+    // 以下のように書いた場合 .value でエラーになる。 変数inputTextがHTMLInputElementであることをtypescriptが判断できないため、valueプロパティの存在を認識しない。
+    // const inputText = this.$refs.inputtext;
+    // inputText.value = "hogehoge";
+
+    // 以下のように書く必要がある
+    const inputText = this.$refs.inputtext as HTMLInputElement;
+    inputText.value = "foo";
+    // 以下は一行書き
+    (this.$refs.inputtext as HTMLInputElement).value = "bar";
   }
 }
 
