5ed9491fe0a94b76c55ec7c3259469203e8cc3e7
ryotakoyama <kryota.gin@gmail.com>
2020-05-03 16:55:11 +0900

    Intersection型。複数のinterface, あるいはtypeエイリアスを統合する

---
+++ src/components/Advance/Intersection.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <p>{{ getCurrentComponentName }}</p>
+    <p>{{ profile }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+
+interface Engineer {
+  name: string;
+  role: string;
+}
+interface Blogger {
+  name: string;
+  follower: number;
+}
+interface EngineerBlogger extends Engineer, Blogger {}
+// type EngineerBlogger = Engineer & Blogger;
+
+@Component
+class Intersection extends Vue implements EngineerBlogger {
+  @Prop({ default: "" })
+  componentName!: string;
+
+  name = "koyama";
+  role = "Frontend";
+  follower = 10000;
+
+  get getCurrentComponentName(): string {
+    return this.componentName;
+  }
+
+  get profile(): string {
+    return `${this.name} : ${this.role} ${this.follower}`;
+  }
+}
+
+export default Intersection;
+</script>
