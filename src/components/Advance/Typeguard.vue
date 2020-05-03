60a7db774db0f2bfd5bda202f360d4ce6fcea2a1
ryotakoyama <kryota.gin@gmail.com>
2020-05-03 19:36:21 +0900

    型を絞り込む手法。typeof, instanceof, in演算子, タグ付きunion

--- src/components/Advance/Typeguard.vue
+++ src/components/Advance/Typeguard.vue
@@ -5,13 +5,63 @@
 <script lang="ts">
 import { Vue, Component, Prop } from "vue-property-decorator";
 
+interface Engineer {
+  name: string;
+  role: string;
+}
+interface Blogger {
+  name: string;
+  follower: number;
+}
+type NomadWorker = Engineer | Blogger;
+
+class Dog {
+  speak() {
+    console.log("Bowwow");
+  }
+}
+
+class Bird {
+  speak() {
+    console.log("Tweet");
+  }
+  fly() {
+    console.log("flutter");
+  }
+}
+
+type Pet = Dog | Bird;
+
 @Component
 class Intersection extends Vue {
   @Prop({ default: "" })
   componentName!: string;
 
-  get getCurrentComponentName() {
+  get getCurrentComponentName(): string {
     return this.componentName;
+  }
+
+  // 普通にtypeofで型を絞り込む
+  toUpperCase(x: string | number) {
+    if (typeof x === "string") {
+      return x.toUpperCase();
+    }
+  }
+  describeProfile(nomadWorker: NomadWorker) {
+    // or演算子でEngineerかBloggerなので重複したプロパティであるnameしか指定できない
+    console.log(nomadWorker.name);
+    // 以下のように書くことで、不確実なプロパティの存在を絞り込むことができる
+    if ("role" in nomadWorker) {
+      console.log(nomadWorker.role);
+    }
+  }
+  havepet(pet: Pet) {
+    // or演算子でDogかBirdなので重複したプロパティであるspeakしか指定できない
+    console.log(pet.speak);
+    // 以下のように書くことで、不確実なクラスタイプを特定できる
+    if (pet instanceof Bird) {
+      pet.fly();
+    }
   }
 }
 
--- src/components/Advance/UnionWithTag.vue
+++ src/components/Advance/UnionWithTag.vue
@@ -5,13 +5,46 @@
 <script lang="ts">
 import { Vue, Component, Prop } from "vue-property-decorator";
 
+class Dog {
+  // リテラル型をタグ用に定義する
+  kind: "Dog" = "Dog";
+  speak() {
+    console.log("Bowwow");
+  }
+}
+
+class Bird {
+  // リテラル型をタグ用に定義する
+  kind: "Bird" = "Bird";
+  speak() {
+    console.log("Tweet");
+  }
+  fly() {
+    console.log("flutter");
+  }
+}
+
+type Pet = Dog | Bird;
+
 @Component
 class Intersection extends Vue {
   @Prop({ default: "" })
   componentName!: string;
 
-  get getCurrentComponentName() {
+  get getCurrentComponentName(): string {
     return this.componentName;
+  }
+
+  havepet(pet: Pet) {
+    // タグを使って条件分岐を行う
+    switch (pet.kind) {
+      case "Dog":
+        console.log("Dog");
+        break;
+      case "Bird":
+        console.log("Bird");
+        break;
+    }
   }
 }
 
