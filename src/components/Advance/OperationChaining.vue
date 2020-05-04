6fc41bc145aa3851043c035acf58c131ad761e98
ryotakoyama <kryota.gin@gmail.com>
2020-05-04 11:55:27 +0900

    ? TypeError: Cannot read property 'name' of undefinedを防ぐ

--- src/components/Advance/OperationChaining.vue
+++ src/components/Advance/OperationChaining.vue
@@ -5,6 +5,33 @@
 <script lang="ts">
 import { Vue, Component, Prop } from "vue-property-decorator";
 
+// OperationChaining もしデータが無かった場合にundefinedを返すようにする
+// すべて見たい場合、すべてに?をつける
+interface DownloadedData {
+  id: number;
+  user?: {
+    name?: {
+      first?: string;
+      last?: string;
+    };
+  };
+}
+
+const donwnloadedData: DownloadedData = {
+  id: 1,
+  user: {
+    name: {}
+  }
+};
+
+// 復習 !をつけないとエラーになる (non-null-asertion)
+// ただし、typescript的にはOKでも実際のjsコードは動かない。"TypeError: Cannot read property 'name' of undefined"
+// console.log(donwnloadedData.user!.name);
+
+// 以下のようにすることで、もしuserのnameのfirstのデータがあれば返す、なければ undefinedを返すというふうに書ける
+// 最後のfirstには?要らない。もしnameがあれば？ までで良いらしい
+console.log(donwnloadedData.user?.name?.first);
+
 @Component
 class Intersection extends Vue {
   @Prop({ default: "" })
