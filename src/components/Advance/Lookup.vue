dd28a437af3ab43c1b622040b61fae9cc1596b71
ryotakoyama <kryota.gin@gmail.com>
2020-05-04 12:05:58 +0900

    interfaceからオブジェクトを掘ってタイプエイリアスを作る(LookUp型)

--- src/components/Advance/Lookup.vue
+++ src/components/Advance/Lookup.vue
@@ -5,6 +5,22 @@
 <script lang="ts">
 import { Vue, Component, Prop } from "vue-property-decorator";
 
+interface DownloadedData {
+  id: number;
+  user: {
+    name?: {
+      first?: string;
+      last?: string;
+    };
+  };
+}
+
+// interfaceからオブジェクトを掘ってタイプエイリアスを作る
+type id = DownloadedData["id"];
+
+const data: id = 1;
+console.log(data);
+
 @Component
 class Intersection extends Vue {
   @Prop({ default: "" })
