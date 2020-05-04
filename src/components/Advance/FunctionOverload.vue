0397d995c5b183cab4b5bb56e75b5057b200c078
ryotakoyama <kryota.gin@gmail.com>
2020-05-04 11:19:56 +0900

    関数のオーバーロード 戻り値がUnion型になってしまうことを防ぐ

--- src/components/Advance/FunctionOverload.vue
+++ src/components/Advance/FunctionOverload.vue
@@ -5,6 +5,19 @@
 <script lang="ts">
 import { Vue, Component, Prop } from "vue-property-decorator";
 
+// 関数のオーバーロード 戻り値がstring | numberになってしまうことを防ぐ。(変数testがnumber あるいは string 型になってることを確認)
+function toUppercase(x: string): string;
+function toUppercase(x: number): number;
+function toUppercase(x: string | number): string | number {
+  if (typeof x === "string") {
+    return x.toUpperCase();
+  } else {
+    return x.toString();
+  }
+}
+
+const test = toUppercase(1);
+
 @Component
 class Intersection extends Vue {
   @Prop({ default: "" })
