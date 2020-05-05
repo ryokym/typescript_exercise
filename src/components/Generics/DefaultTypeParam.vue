c4c43b9668a693435cb501fdd0aeb14532185e33
ryotakoyama <kryota.gin@gmail.com>
2020-05-05 17:25:54 +0900

    ジェネリクス(型引数)の初期値を指定する

--- src/components/Generics/DefaultTypeParam.vue
+++ src/components/Generics/DefaultTypeParam.vue
@@ -8,6 +8,19 @@
 <script lang="ts">
 import { Vue, Component, Prop } from "vue-property-decorator";
 
+// ジェネリクスに初期値を与える anyでもできる
+interface ResponseData<T = string | number> {
+  msg: T;
+  status: number;
+}
+// 初期値がない場合は下記はエラーとなる
+type tmp = ResponseData;
+
+const data: ResponseData<string> = {
+  msg: "sample",
+  status: 1
+};
+
 @Component
 class Intersection extends Vue {
   @Prop({ default: "" })
