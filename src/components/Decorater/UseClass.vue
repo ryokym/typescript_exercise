4413f813b02feb419ee4dd07d2542823c86ff291
ryotakoyama <kryota.gin@gmail.com>
2020-05-05 18:24:31 +0900

    デコレータを単純に定義する

---
+++ src/components/Decorater/UseClass.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <p>{{ getCurrentComponentName }}</p>
+    <p></p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+
+// デコレーターの引数にクラスの型を取りたい、が、汎用性を持たせたい。クラス → 実際はconstructor関数を指す。関数なのでFunction型を取る
+function Logging(constructor: Function) {
+  console.log("loggingStart!");
+}
+
+@Component
+// 関数の定義時に実行される
+@Logging
+class Intersection extends Vue {
+  @Prop({ default: "" })
+  componentName!: string;
+
+  get getCurrentComponentName(): string {
+    return this.componentName;
+  }
+}
+
+export default Intersection;
+</script>
