03c9805697b1f1c0dd5184492abaa3f535b2696b
ryotakoyama <kryota.gin@gmail.com>
2020-05-05 15:33:31 +0900

    ジェネリクス、引数や戻り値にオブジェクトを指定する際にタイプエイリアスをextendsすると構文が冗長にならずに済む

---
+++ src/components/Generics/Extends.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <p>{{ getCurrentComponentName }}</p>
+    <p></p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+// タイプエイリアスを作成
+type name = {
+  name: string;
+};
+// type nameを継承した型引数
+function copy<T extends name>(value: T): T {
+  return value;
+}
+console.log(copy({ name: "r_koyama" }));
+
+@Component
+class Intersection extends Vue {
+  @Prop({ default: "" })
+  componentName!: string;
+
+  get getCurrentComponentName(): string {
+    return this.componentName;
+  }
+}
+
+export default Intersection;
+</script>
