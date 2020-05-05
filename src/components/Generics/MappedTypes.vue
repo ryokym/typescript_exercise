2b707f39603a0b5bc009108efe0763c708f6346a
ryotakoyama <kryota.gin@gmail.com>
2020-05-05 17:51:17 +0900

    mappedTypes, オブジェクトのタイプエイリアスの一行書き

--- src/components/Generics/MappedTypes.vue
+++ src/components/Generics/MappedTypes.vue
@@ -8,6 +8,29 @@
 <script lang="ts">
 import { Vue, Component, Prop } from "vue-property-decorator";
 
+// mappedTypes一行で簡潔に書ける。
+type mappedTypes = {
+  [P in "apple" | "banana" | "orange"]: string;
+};
+// 上記と同じ これが上記なら一行で書けるっていうこと
+type objectTypes = {
+  apple: string;
+  banana: string;
+  orange: string;
+};
+
+// 全部readonlyにする
+type mappedTypes2 = {
+  readonly [P in "apple" | "banana" | "orange"]: string;
+};
+
+// 全部オプショナル化する
+type mappedTypes3 = {
+  [P in "apple" | "banana" | "orange"]?: string;
+};
+
+// ちなみにreadonlyや?の前に-を付けるとすべて外すという意味になる
+
 @Component
 class Intersection extends Vue {
   @Prop({ default: "" })
